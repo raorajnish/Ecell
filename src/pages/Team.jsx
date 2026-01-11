8874fb727e7481cd705063844842d9fa640d2543
Sahil D <114382046+Sahildk@users.noreply.github.com>
2026-01-12 03:21:39 +0530

    Revert "Reapply "added teams page + fixed events page""
    This reverts commit f82661758fc7f7227f59b62039129be5fe897381.

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import TeamContent from '../components/teams/Teams'
-import Footer from '../components/home/Main-footer'
 
 const Team = () => {
   return (
-    <div>
-      <TeamContent />
-      <Footer />
+    <div className="bg-black h-screen w-screen flex flex-col justify-center items-center text-5xl  lg:text-9xl text-white font-[vampire]">
+      Team Page
+      <p className='text-sm font-[font1]' >Coming soon...</p>
     </div>
   );
 }
